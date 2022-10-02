4d5041f65c1b059be8a0f5e66a59ef3ab3e61e1e
Erikas Staugas <erikas.staugas@kcl.ac.uk>
2022-10-02 20:54:04 +0100

    Sequence of n prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+    
+    number = 0
+    while len(list) < number_of_primes: 
+        number += 1
+        divisor = 2 
+
+        if(number == 2):
+            list.append(number)
+        
+        if(number % 2 != 0):   
+            is_prime = False         
+            for divisor in range(2, number): 
+                print(divisor)
+                if(number % divisor == 0):
+                    is_prime = False
+                    break
+                else:
+                    is_prime = True
+            
+            if(is_prime):
+                list.append(number)
     return list
+
+print(primes(10))
